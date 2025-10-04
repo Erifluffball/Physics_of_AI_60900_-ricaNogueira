a367d29f284c0b5a8a052fdfd903b23323339c98
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-10-04 15:46:51 +0100

    Update MoveShell.cs

--- Assets/Scripts/MoveShell.cs
+++ Assets/Scripts/MoveShell.cs
@@ -1,6 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveShell
 {
-    
+
+	float speed = 1.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+    	this.transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+
 }
