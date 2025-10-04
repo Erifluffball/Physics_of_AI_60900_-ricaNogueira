026f8b3ccb5815831fb97806a9160aca13bb94f1
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-10-04 16:05:32 +0100

    Velocity Tutorial Finished

--- Assets/Scripts/MoveShell.cs
+++ Assets/Scripts/MoveShell.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveShell
+public class MoveShell : MonoBehaviour
 {
 
 	float speed = 1.0f;
@@ -14,7 +14,7 @@ public class MoveShell
 
     void Update()
     {
-    	this.transform.Translate(0, 0, Time.deltaTime * speed);
+    	this.transform.Translate(0, Time.deltaTime * speed * 0.5f, Time.deltaTime * speed);
     }
 
 
